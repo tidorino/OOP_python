a360af1ef4723b2ee9b5a86073331b29f4c02f72
tidorino <tidorino@hotmail.com>
2022-07-07 12:21:05 +0300

    class exercises - encapsulation

---
+++ encapsulation_lab/02_mammal.py
@@ -0,0 +1,31 @@
+class Mammal:
+    __kingdom = 'animals'
+
+    def __init__(self, name, animal_type, sound):
+        self.name = name
+        self.type = animal_type
+        self.sound = sound
+
+    def make_sound(self):
+        """
+        returns a string in the format "{name} makes {sound}"
+        """
+        return f'{self.name} makes {self.sound}'
+
+    def get_kingdom(self):
+        """
+        returns the private kingdom attribute
+        """
+        return self.__kingdom
+
+    def info(self):
+        """
+        returns a string in the format "{name} is of type {type}"
+        """
+        return f'{self.name} is of type {self.type}'
+
+
+mammal = Mammal("Dog", "Domestic", "Bark")
+print(mammal.make_sound())
+print(mammal.get_kingdom())
+print(mammal.info())
