f53bff567634d7bd1bf287ff0314dd4ebb5f9ae6
tidorino <tidorino@hotmail.com>
2022-07-13 17:45:50 +0300

    class exercises

---
+++ static_and_class_methods_exercise/01_photo_album.py
@@ -0,0 +1,58 @@
+from math import ceil
+
+
+class PhotoAlbum:
+    PHOTOS_PER_PAGE = 4
+
+    def __init__(self, pages):
+        self.pages = pages
+        self.photos = self.__init_photos(pages)
+        # self.photos = [['[]' for p in range(self.PHOTOS_PER_PAGE)] for _ in range(self.pages)]
+
+    def __init_photos(self, pages):
+        result = []
+        for _ in range(pages):
+            result.append([])
+        return result
+
+    """"
+        self.photos = [[' ' for p in range(self.PHOTOS_PER_PAGE)] for _ in range(self.pages)]
+        # -> empty matrix) representing the album with its pages where you should put the photos
+        # Each page can contain only 4 photos
+    """
+    @classmethod
+    def from_photos_count(cls, photos_count: int):
+        pages = ceil(photos_count / PhotoAlbum.PHOTOS_PER_PAGE)
+        return cls(pages)
+
+    def add_photo(self, label):
+        for idx, page in enumerate(self.photos):
+            if len(page) < PhotoAlbum.PHOTOS_PER_PAGE:
+                page.append(label)
+                return f'{label} photo added successfully on page {idx + 1} slot {len(page)}'
+        return 'No more free slots'
+
+    def display(self):
+        separator = '-' * 11
+        result = separator + '\n'
+        for page in self.photos:
+            result += ' '.join(['[]' for _ in page]) + '\n'
+            result += separator + '\n'
+
+        return result.strip()
+
+
+album = PhotoAlbum(2)
+
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+
+print(album.display())
+
+
+
