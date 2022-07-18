653516fbce388a491ae1ecdbd45c0c7374d9af21
tidorino <tidorino@hotmail.com>
2022-07-18 23:51:04 +0300

    class exercises

--- polymorphism_and_abstraction_exercise/02_groups.py
+++ polymorphism_and_abstraction_exercise/02_groups.py
@@ -3,7 +3,7 @@ class Person:
         self.name = name
         self.surname = surname
 
-    def __repr__(self):
+    def __str__(self):
         return f'{self.name} {self.surname}'
 
     def __add__(self, other):
@@ -19,10 +19,14 @@ class Group:
         return len(self.people)
 
     def __add__(self, other):
-        return Group(f'{self.name}, {other.name}', self.people + other.people)
+        new_name = f'{self.name} {other.name}'
+        merged_members = self.people + other.people
+        return Group(new_name, merged_members)
+        # return Group(f'{self.name}, {other.name}', self.people + other.people)
 
-    def __repr__(self):
-        return f'Group {self.name} with members {", ".join([str(x) for x in self.people])}'
+    def __str__(self):
+        people_str = ", ".join([str(x) for x in self.people])
+        return f'Group {self.name} with members {people_str}'
 
     def __getitem__(self, idx):
         return f'Person {idx}: {str(self.people[idx])}'
---
+++ polymorphism_and_abstraction_exercise/05_animals/project/cat.py
@@ -0,0 +1,15 @@
+from project.animal import Animal
+
+
+class Cat(Animal):
+    def __init__(self, name, age, gender):
+        super().__init__(name, age, gender)
+
+    def make_sound(self):
+        return f'Meow meow!'
+
+
+
+
+
+
