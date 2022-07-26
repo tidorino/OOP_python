681f8e461bb57ba8674987dc826c162b02fa5c17
tidorino <tidorino@hotmail.com>
2022-07-26 16:55:58 +0300

    decorators exercises

---
+++ decorators_lab/04_multiply.py
@@ -0,0 +1,27 @@
+from functools import wraps
+
+
+def multiply(n):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return result * n
+
+        return wrapper
+
+    return decorator
+
+
+# @multiply(3)
+# def add_ten(number):
+#     return number + 10
+#
+
+# print(add_ten(3))
+@multiply(5)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(6))
