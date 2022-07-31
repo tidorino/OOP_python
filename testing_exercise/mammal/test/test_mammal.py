0f48484161cf1f5373a9594780bba4a01f6d46b5
tidorino <tidorino@hotmail.com>
2022-07-31 21:02:20 +0300

    unit test exercises

---
+++ testing_exercise/mammal/test/test_mammal.py
@@ -0,0 +1,38 @@
+from project.mammal import Mammal
+
+from unittest import TestCase, main
+
+
+class MammalTest(TestCase):
+
+    def test_worker_is_initialized_correctly(self):
+        mammal = Mammal('Niko', 'hippo', 'huu')
+
+        self.assertEqual('Niko', mammal.name)
+        self.assertEqual('hippo', mammal.type)
+        self.assertEqual('huu', mammal.sound)
+        self.assertEqual('animals', mammal._Mammal__kingdom)
+
+    def test_make_sound(self):
+        mammal = Mammal('Niko', 'hippo', 'huu')
+
+        actual_result = mammal.make_sound()
+        expected_result = f"{mammal.name} makes {mammal.sound}"
+        self.assertEqual(expected_result, actual_result)
+
+    def test_get_kingdom(self):
+        mammal = Mammal('Niko', 'hippo', 'huu')
+
+        actual_result = mammal.get_kingdom()
+        expected_result = 'animals'
+        self.assertEqual(expected_result, actual_result)
+
+    def test_info__returns_proper_string(self):
+        mammal = Mammal('Niko', 'hippo', 'huu')
+        actual_result = mammal.info()
+        expected_result = f"{mammal.name} is of type {mammal.type}"
+        self.assertEqual(expected_result, actual_result)
+
+
+if __name__ == '__main__':
+    main()
