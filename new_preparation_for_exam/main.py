02726aaac3cf87bbacab09776dd2c488f73e1345
tidorino <tidorino@hotmail.com>
2023-04-04 12:32:31 +0300

    update

--- new_preparation_for_exam/main.py
+++ new_preparation_for_exam/main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+from project.controller import Controller
+from project.player import Player
+from project.supply.drink import Drink
+from project.supply.food import Food
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+controller = Controller()
+apple = Food("apple", 22)
+cheese = Food("cheese")
+juice = Drink("orange juice")
+water = Drink("water")
+first_player = Player('Peter', 15)
+second_player = Player('Lilly', 12, 94)
+print(controller.add_supply(cheese, apple, cheese, apple, juice, water, water))
+print(controller.add_player(first_player, second_player))
+print(controller.duel("Peter", "Lilly"))
+print(controller.add_player(first_player))
+print(controller.sustain("Lilly", "Drink"))
+first_player.stamina = 0
+print(controller.duel("Peter", "Lilly"))
+print(first_player)
+print(second_player)
+controller.next_day()
+print(controller)
--- new_preparation_for_exam/project/controller.py
+++ new_preparation_for_exam/project/controller.py
@@ -16,9 +16,9 @@ class Controller:
         return f'Successfully added: {", ".join(added_players)}'
 
     def add_supply(self, *supplies):
-        for supply in supplies:
-            self.supplies.append(supply)
-        # self.supplies.extend(supplies)
+        # for supply in supplies:
+        #     self.supplies.append(supply)
+        self.supplies.extend(supplies)
 
     def sustain(self, player_name, sustenance_type):
         player = self.__find_player_by_name(player_name)
@@ -37,13 +37,35 @@ class Controller:
         return f'{player_name} sustained successfully with {supply.name}.'
 
     def duel(self, first_player_name, second_player_name):
-        pass
+        first_player = self.__find_player_by_name(first_player_name)
+        second_player = self.__find_player_by_name(second_player_name)
+
+        error_message = self.__get_error_message(first_player, second_player)
+        if error_message:
+            return error_message
+
+        if second_player.stamina < first_player.stamina:
+            first_player, second_player = second_player, first_player
+        
+        first_player_win = self.__attack(first_player, second_player)
+        if first_player_win:
+            return f'Winner: {first_player.name}'
+        second_player_win = self.__attack(second_player, first_player)
+        if second_player_win:
+            return f'Winner: {second_player.name}'
+
+        winner = first_player if first_player.stamina > second_player.stamina else second_player
+        return f'Winner: {winner.name}'
 
     def next_day(self):
-        pass
+        for player in self.players:
+            player.stamina = max(player.stamina - player.age * 2, 0)
+            self.sustain(player.name, 'Food')
+            self.sustain(player.name, 'Drink')
 
     def __str__(self):
-        return ''
+        return '\n'.join([str(x) for x in self.players]) + '\n' + \
+               '\n'.join([x.details() for x in self.supplies])
 
     def __find_player_by_name(self, player_name):
         for player in self.players:
@@ -55,6 +77,24 @@ class Controller:
             supply = self.supplies[idx]
             if supply.__class__.__name__ == sustenance_type:
                 return idx, supply
+        return - 1, None
+
+    @staticmethod
+    def __get_error_message(first_player, second_player):
+        error_message = ''
+        if first_player.stamina == 0:
+            error_message += f'Player {first_player.name} does not have enough stamina.'
+        if second_player.stamina == 0:
+            error_message += '\n' + f'Player {second_player.name} does not have enough stamina.'
+
+        return error_message.strip()
+
+    @staticmethod
+    def __attack(attacker, enemy):
+        attacker_damage = attacker.stamina / 2
+        enemy.stamina = max(enemy.stamina - attacker_damage, 0)
+        return enemy.stamina == 0
+
 
 
 
